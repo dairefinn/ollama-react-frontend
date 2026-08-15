b5b98fd479f6352d9c2d2da4a2a159d853ff4375
Daire Finn <dmanfinn@gmail.com>
2026-08-15 22:33:10 +0100

    Nav update

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,13 +1,14 @@
 import './Navigation.css';
 
 import { NavLink } from "react-router";
+import { ChatsCircle, MagnifyingGlass } from "@phosphor-icons/react";
 
 function Navigation() {
     return (
         <>
             <nav className="navigation">
-                <NavLink className='navigation-link' to="/chat">Chat</NavLink>
-                <NavLink className='navigation-link' to="/query">Query</NavLink>
+                <NavLink className='navigation-link' to="/chat"><ChatsCircle size={18} />Chat</NavLink>
+                <NavLink className='navigation-link' to="/query"><MagnifyingGlass size={18} />Query</NavLink>
             </nav>
         </>
     )
